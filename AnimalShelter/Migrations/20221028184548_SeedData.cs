5704b878c2a3637b70fdefb151395ec59d6864ef
josephjacksonw <josephjacksonw@gmail.com>
2022-10-28 15:47:42 -0400

    built seeded data

---
+++ AnimalShelter/Migrations/20221028184548_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 1, "Max", "Cat" },
+                    { 2, 8, "Laura", "Dog" },
+                    { 3, 6, "Marley", "Dog" },
+                    { 4, 5, "Cheese", "Cat" },
+                    { 5, 12, "Grouch", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -8,7 +8,17 @@ namespace AnimalShelter.Models
             : base(options)
         {
         }
-
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+              .HasData(
+                  new Animal { AnimalId = 1, Name = "Max", Species = "Cat", Age = 1 },
+                  new Animal { AnimalId = 2, Name = "Laura", Species = "Dog", Age = 8 },
+                  new Animal { AnimalId = 3, Name = "Marley", Species = "Dog", Age = 6 },
+                  new Animal { AnimalId = 4, Name = "Cheese", Species = "Cat", Age = 5 },
+                  new Animal { AnimalId = 5, Name = "Grouch", Species = "Dog", Age = 12 }
+              );
+        }
         public DbSet<Animal> Animals { get; set; }
     }
 }
